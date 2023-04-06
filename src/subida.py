8bdbb3265d37a9a1f10b2e7981c81115894df9c4
agustin aguero <aaguero@agustins-MacBook-Pro.local>
2023-04-06 10:51:53 -0300

    Constants created

--- src/subida.py
+++ src/subida.py
@@ -8,24 +8,14 @@ import time
 import traceback
 
 from driver_email import enviar_mail_con_adjuntos
-
-PROGRAMADOR = 'xxxxxxxxxxxxx@xxxxxxxxxxxxx.com'
-USER = 'xxxxxxxxxxxxx@xxxxxxxxxxxxx.com'
-PASSWORD = 'xxxxxxxxxxxxx'
-ENCABEZADO_SUBIDA_DATOS = [
-    "ID Cuenta o Nro. de Asig. (0)", "ID SubCliente (1)", "Activa (S, N) (2)",
-    "ID Supervisor (3)", "ID Ejecutivo (4)", "Nº de Asignación Nuevo (5)", "Fecha de Contacto (6)",
-    "ID Acción (7)", "ID Resultado (8)", "Notas (9)", "ID Usuario (10)", "ID SubEstado (11)",
-    "ID SubCliente (12)", "Importe Asignado (13)", "Importe Histórico (14)",
-    "Observaciones (15)", "Email (16)", "ID Tipo de Teléfono (17)", "Nro. de Teléfono (18)",
-    "Obs. de Teléfono (19)", "ID Tipo de Domicilio (20)", "Domicilio sin Formato (21)",
-    "Obs. de Domicilio (22)", "ID Localidad (23)", "ID Provincia (24)", "Fecha de Vencimiento (25)",
-    "Número de Referencia (26)", "Importe Factura (27)", "Observaciones Factura (28)", "ID Sucursal (29)",
-    "Fecha de Pago (30)", "ID Concepto (31)", "Importe de Pago(32)", "ID Usuario  de Pago(33)",
-    "Rendido (S, N) (34)", "Fecha Proximo Pago (35)", "Importe Proximo Pago (36)", "Fecha de Acuerdo (37)",
-    "Días de Vencimiento (38)", "Importe de Acuerdo (39)", "Sueldo (40)", "Cantidad de Vehiculos (41)",
-    "Datos Patrimoniales (42)"
-]
+from .constants.constants import (
+    PROGRAMADOR,
+    USER,
+    PASSWORD,
+    ENCABEZADO_SUBIDA_DATOS,
+    PROVINCIAS,
+    COLUMNAS_UTILES_COMAFI,
+)
 
 
 def limpiar_numeros(df_num):
@@ -97,31 +87,7 @@ def Preparacion_Cuentas():
                 'Importe Historico (14)', 'Observaciones (15)', 'Fecha Fin de Gestion (16)', 'IDSucursal(17)']
     df_os = pd.DataFrame(columns=columnas)
 
-    provincias = {
-            'BUENOS AIRES':	'24',
-            'CAPITAL FEDERAL': '23',
-            'CATAMARCA': '22',
-            'CHACO': '21',
-            'CHUBUT': '20',
-            'CORDOBA': '19',
-            'CORRIENTES': '18',
-            'ENTRE RIOS': '17',
-            'FORMOSA': '16',
-            'JUJUY': '15',
-            'LA PAMPA': '14',
-            'LA RIOJA': '13',
-            'MENDOZA': '12',
-            'MISIONES': '11',
-            'NEUQUEN': '10',
-            'RIO NEGRO': '9',
-            'SALTA': '8',
-            'SAN JUAN': '7',
-            'SAN LUIS': '6',
-            'SANTA CRUZ': '5',
-            'SANTIAGO DEL ESTERO': '4',
-            'TIERRA DEL FUEGO': '3',
-            'SANTA FE':	'2',
-            'TUCUMAN': '1'}
+    provincias = PROVINCIAS
 
     date_now = datetime.date.today()
     years_to_add = date_now.year + 3
@@ -176,50 +142,8 @@ def Preparacion_Cuentas_Comafi():
     # lectura planilla modelo
     df_os = pd.read_csv('modelos/modelo_cuentas.csv', encoding='ANSI', sep=';')
     df = pd.read_excel('emerix.xlsx', dtype=str)
-    col_utiles = {
-        'Nº Doc': 'dni',
-        'Apellido, Nombre': 'nombre',
-        'Direccion': 'direccion',
-        'Localidad': 'localidad',
-        'Cod. Pos.': 'cod_postal',
-        'Provincia': 'provincia',
-        'Telefono': 'telefono',
-        'Sucursal': 'sucursal',
-        'Banca': 'banca',
-        'Cod. Linea': 'cod_linea',
-        'Linea': 'linea',
-        'Deuda Vencida': 'deuda_total',
-        'Cap.': 'deuda_capital',
-        'Dias Mora': 'dias_mora',
-        'Inicio mora': 'fecha_inicio',
-        'Fecha Ult. Pago': 'fecha_ult_pago',
-        'Subcliente': 'subcliente'}
-    provincias = {
-            '0': '0',
-            'BUENOS AIRES':	'24',
-            'CAPITAL FEDERAL': '23',
-            'CATAMARCA': '22',
-            'CHACO': '21',
-            'CHUBUT': '20',
-            'CORDOBA': '19',
-            'CORRIENTES': '18',
-            'ENTRE RIOS': '17',
-            'FORMOSA': '16',
-            'JUJUY': '15',
-            'LA PAMPA': '14',
-            'LA RIOJA': '13',
-            'MENDOZA': '12',
-            'MISIONES': '11',
-            'NEUQUEN': '10',
-            'RIO NEGRO': '9',
-            'SALTA': '8',
-            'SAN JUAN': '7',
-            'SAN LUIS': '6',
-            'SANTA CRUZ': '5',
-            'SANTIAGO DEL ESTERO':	'4',
-            'TIERRA DEL FUEGO': '3',
-            'SANTA FE':	'2',
-            'TUCUMAN':	'1'}
+    col_utiles = COLUMNAS_UTILES_COMAFI
+    provincias = PROVINCIAS
 
     df = df[list(col_utiles.keys())]
     df = df.rename(columns=col_utiles)
@@ -379,25 +303,7 @@ def Preparacion_Datos_Comafi():
     df_subida = pd.read_csv('modelos/modelo_datos.csv', encoding='ANSI', sep=';')
 
     df_num = pd.read_excel('emerix.xlsx', dtype=str)
-    col_utiles = {
-        'Nº Doc': 'dni',
-        'Apellido, Nombre': 'nombre',
-        'Direccion': 'direccion',
-        'Localidad': 'localidad',
-        'Cod. Pos.': 'cod_postal',
-        'Provincia': 'provincia',
-        'Telefono': 'telefono',
-        'Sucursal': 'sucursal',
-        'Banca': 'banca',
-        'Cod. Linea': 'cod_linea',
-        'Linea': 'linea',
-        'Deuda Vencida': 'deuda_total',
-        'Cap.': 'deuda_capital',
-        'Dias Mora': 'dias_mora',
-        'Inicio mora': 'fecha_inicio',
-        'Fecha Ult. Pago': 'fecha_ult_pago',
-        'Subcliente': 'subcliente'
-    }
+    col_utiles = COLUMNAS_UTILES_COMAFI
     df_num = df_num[list(col_utiles.keys())]
     df_num = df_num.rename(columns=col_utiles)
     df_num = df_num[df_num['telefono'].notna()]
