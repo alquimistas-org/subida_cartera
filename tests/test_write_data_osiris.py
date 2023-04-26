35215b61a0dc6747c30b771828c257289b1d2917
mbertani <mbertani@fu.do>
2023-04-26 18:52:03 -0300

    tests/test_write_data_osiris.py: addap test to previous changes
    mostly with os.getenv mocks and delete one unnecesary tets

--- tests/test_write_data_osiris.py
+++ tests/test_write_data_osiris.py
@@ -1,7 +1,6 @@
 from freezegun import freeze_time
 import pandas as pd
 import pytest
-import tempfile
 from unittest import mock
 from src.write_data_osiris import Escribir_Datos_Osiris
 
@@ -30,30 +29,9 @@ from src.write_data_osiris import Escribir_Datos_Osiris
 )
 class TestWriteDataOsiris:
 
-    @mock.patch('src.write_data_osiris.os.getenv')
+    @freeze_time("2023-04-08 18:45:00")
     def test_create_file_in_existing_folder_get_by_env_variable(
         self,
-        mock_get_env,
-        mock_df,
-        mock_filename,
-        mock_cols_df,
-        mock_cols_osiris,
-    ):
-
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            mock_get_env.return_value = tmpdirname
-            Escribir_Datos_Osiris(
-                df=mock_df,
-                filename=mock_filename,
-                cols_df=mock_cols_df,
-                cols_osiris=mock_cols_osiris,
-            )
-
-    @freeze_time("2023-04-08 18:45:00")
-    @mock.patch.object(pd.DataFrame, 'to_csv')
-    def test_create_file_in_existing_folder_without_env_variable(
-        self,
-        mock_to_csv,
         mock_df,
         mock_filename,
         mock_cols_df,
@@ -61,32 +39,23 @@ class TestWriteDataOsiris:
     ):
 
         Escribir_Datos_Osiris(
-            df=mock_df,
-            filename=mock_filename,
-            cols_df=mock_cols_df,
-            cols_osiris=mock_cols_osiris,
-        )
+                df=mock_df,
+                filename=mock_filename,
+                cols_df=mock_cols_df,
+                cols_osiris=mock_cols_osiris,
+            )
 
-        mock_to_csv.assert_called_once()
-        mock_to_csv.assert_called_once_with(
-            f'{"Subida Osiris"}/(18.45 hs) - {mock_filename}',
-            sep=';',
-            index=False,
-            encoding='latin_1'
-        )
-
-    @mock.patch('src.write_data_osiris.os.getenv')
+    @mock.patch('src.write_data_osiris.there_is_not_saved_files_directory')
     def test_raise_exception_folder_doesnt_exist(
         self,
-        mock_get_env,
+        mock_there_is_not_saved_files_directory,
         mock_df,
         mock_filename,
         mock_cols_df,
         mock_cols_osiris,
     ):
-
+        mock_there_is_not_saved_files_directory.return_value = True
         with pytest.raises(Exception):
-            mock_get_env.return_value = "Another folder"
             Escribir_Datos_Osiris(
                 df=mock_df,
                 filename=mock_filename,
