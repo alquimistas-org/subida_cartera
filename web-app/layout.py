c9c39439bf602fd0486e9d30f5eea6f0b852bcce
natilou <natali.fpaura@gmail.com>
2023-05-11 12:01:59 -0300

    Refresh data button
    - Adding clear data component
    - Adding callback to the clear data button and to trigger a modal to confirm the action
    - When we click the button to confirm the deletion, the page is refreshed which clears the data from dcc.Store
    - Adding storage_type='memory' to dcc.Store

--- web-app/layout.py
+++ web-app/layout.py
@@ -1,11 +1,13 @@
 from dash import html, dcc
 # import dash_bootstrap_components as dbc
+from components.clear_data import ClearData
 from components.client_button import ClientButton
 from components.upload import Upload
 
 
 client_button = ClientButton()
 upload = Upload()
+clear_data = ClearData()
 
 app_layout = html.Div([
     html.H1(
@@ -16,11 +18,16 @@ app_layout = html.Div([
             'marginLeft': '10px',
             'marginTop': '10px'
         }),
+    clear_data.create(),
+    html.Div([
+        html.P("Selecciona un cliente", className="selec-client")
+    ]),
     client_button.create(),
     html.Div(id='client-selected-value', style={'display': 'none'}),
     upload.create("1. Subir archivo para preparación de cuentas", id="cr", multiple_files=False),
     html.Div([
-                html.Div([], id="div-download", className="donwload-container")
-            ], id="major-div-download", className="major-donwload-container"),
-    dcc.Store(id='stored-dfs'),
+                html.Div([], id='div-download', className='donwload-container')
+            ], id='major-div-download', className='major-donwload-container'),
+    dcc.Store(id='stored-dfs', clear_data=False, storage_type='memory'),
+    dcc.Location(id='url', refresh=True),
 ])
