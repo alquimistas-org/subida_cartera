3b675a6fee7743a322ae84b53bad358c102ce652
Matias Bertani <matiasbertani@gmail.com>
2023-06-03 22:26:39 -0300

    web-app: add needed callbacks and helpers to preparae data_provider data

--- web-app/callbacks_helpers.py
+++ web-app/callbacks_helpers.py
@@ -1,12 +1,22 @@
 import io
-from dash import html, dcc
 from pathlib import Path
-import dash_bootstrap_components as dbc
+from typing import List, Tuple
+
+from dash import (
+    ctx,
+    dcc,
+    html,
+)
 from dash.exceptions import PreventUpdate
-from src.subida import Preparacion_Cuentas
-from src.prepare_comafi_accounts import prepare_comafi_accounts
+import dash_bootstrap_components as dbc
+
+from components.download_button import DownloadButton
 from src.adapters.dash_dataframe_saver import DashDataFrameSaver
 from src.constants.constants import ACCOUNTS_MODEL_CSV_PATH
+from src.data_info import GenerateDataInfo
+from src.prepare_comafi_accounts import prepare_comafi_accounts
+from src.risk_data import risk_data
+from src.subida import Preparacion_Cuentas
 
 
 def process_naranja_client(dash_dataframe_saver: DashDataFrameSaver, decoded, content_string):
@@ -97,3 +107,46 @@ def display_modal_error(client_selected, filename):
         [dbc.Button("Aceptar", id="accept-btn-error")],
     )
     return True, [modal_header, modal_body, modal_footer]
+
+
+def get_id_and_value_from_context():
+    if not ctx.triggered:
+        raise PreventUpdate
+    triggered_id = ctx.triggered_id
+    triggered_input_value = ctx.triggered[0]['value']
+    triggered_state_value = [value for key, value in ctx.states.items() if triggered_id.get('id') in key][0]
+    return triggered_id, triggered_input_value, triggered_state_value
+
+
+def process_external_provider_data(
+    dash_dataframe_saver: DashDataFrameSaver,
+    osiris_accounts_data: dict,
+    external_provider_data: dict,
+    external_provider_name: str,
+) -> Tuple[List[DownloadButton], dict]:
+    if external_provider_name == 'riesgo-online':
+        risk_data(
+            risk_file_path=external_provider_data,
+            osiris_accounts_file_path=osiris_accounts_data,
+            dataframe_saver=dash_dataframe_saver
+        )
+    else:
+        GenerateDataInfo.process(
+            osiris_accounts_data=osiris_accounts_data,
+            external_provider_data=external_provider_data,
+            dataframe_saver=dash_dataframe_saver
+        )
+
+    dfs = dash_dataframe_saver.get_saved_dfs()
+
+    if not dfs:
+        raise PreventUpdate
+
+    download_buttons = [
+        DownloadButton.create(df_name)
+        for df_name in dfs.keys()
+    ]
+
+    result_data = {df_name: df.to_csv(index=False) for df_name, df in dfs.items()}
+
+    return download_buttons, result_data
